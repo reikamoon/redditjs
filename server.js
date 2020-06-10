e609a1e969bdecaafc5f1b41d71ef0376cab35c7
Anjelica <ambrosio.anjelica@gmail.com>
2020-06-10 13:14:20 -0700

    added layout for new post

--- server.js
+++ server.js
@@ -2,21 +2,34 @@
 const express = require('express')
 const app = express()
 
-// Require Handlebars
-const handlebars = require('handlebars');
-const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
+// require handlebars
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({
-    defaultLayout: 'main',
-    handlebars: allowInsecurePrototypeAccess(handlebars),
-});
 
-app.engine('handlebars', hbs.engine); 
+// Use "main" as our default layout
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+// Use handlebars to render
 app.set('view engine', 'handlebars');
+
+// Require Handlebars
+// const handlebars = require('handlebars');
+// const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
+// const exphbs = require('express-handlebars');
+// const hbs = exphbs.create({
+//     defaultLayout: 'main',
+//     handlebars: allowInsecurePrototypeAccess(handlebars),
+// });
+//
+// app.engine('handlebars', hbs.engine); 
+// app.set('view engine', 'handlebars');
 
 // INDEX
   app.get('/', (req, res) => {
     res.render('home', {msg: 'Nice!'});
+  })
+
+// New posts
+  app.get('/posts/new',(req, res) => {
+    res.render('posts-new', {});
   })
 
 
