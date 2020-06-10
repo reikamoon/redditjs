81ec46c75f2d7bd5e3d872544e3c23b58e112884
Anjelica <ambrosio.anjelica@gmail.com>
2020-06-10 13:03:47 -0700

    added home layout and navbar

--- server.js
+++ server.js
@@ -2,9 +2,21 @@
 const express = require('express')
 const app = express()
 
+// Require Handlebars
+const handlebars = require('handlebars');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    handlebars: allowInsecurePrototypeAccess(handlebars),
+});
+
+app.engine('handlebars', hbs.engine); 
+app.set('view engine', 'handlebars');
+
 // INDEX
   app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('home', {msg: 'Nice!'});
   })
 
 
