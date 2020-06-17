eeca018a6aba34e46e99347d503b0a2cd573c6fd
Anjelica <ambrosio.anjelica@gmail.com>
2020-06-17 12:37:34 -0700

    added controllers

---
+++ controllers/posts.js
@@ -0,0 +1,32 @@
+const app = require('express')()
+const Post =  require('../models/post')
+
+// Post index
+app.get('/' , (req, res) => {
+    console.log("in index")
+    Post.find({}).lean()
+    .then(posts => {
+      res.render("posts-index", { posts });
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+});
+
+// NEW
+app.get('/posts/new', (req, res) => {
+    console.log("in posts-new")
+    return res.render('posts-new', {});
+  })
+
+
+// Create
+app.post('/posts/new', (req, res) => {
+    console.log(req.body);
+
+    const post = new Post(req.body)
+
+    post.save((err, body) => {
+        return res.redirect(`/`)
+    })
+})
