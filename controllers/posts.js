8650e773515ed5eea49d3b23829c541ac42413db
Anjelica <ambrosio.anjelica@gmail.com>
2020-06-18 18:14:40 -0700

    users must be signed in to submit a post

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,45 +3,46 @@ const Post = require('../models/post');
 module.exports = app => {
 
   // INDEX
-  app.get('/' , (req, res) => {
-      console.log("in index")
-      Post.find({}).lean()
-      .then(posts => {
-        res.render("posts-index", { posts });
-      })
-      .catch(err => {
-        console.log(err.message);
-      });
-  });
+  app.get("/", (req, res) => {
+  var currentUser = req.user;
 
-  // NEW
+  Post.find({})
+    .then(posts => {
+      res.render("posts-index", { posts, currentUser });
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+});
+
+// NEW
     app.get('/posts/new',(req, res) => {
       console.log("Posts-New")
       return res.render('posts-new', {});
     })
 
-  // CREATE
-    app.post('/posts/new', (req, res) => {
-      // INSTANTIATE INSTANCE OF POST MODEL
-      const post = new Post(req.body);
-      console.log(post)
+// CREATE
+  app.post("/posts/new", (req, res) => {
+    if (req.user) {
+      var post = new Post(req.body);
 
-      // SAVE INSTANCE OF POST MODEL TO DB
-      post.save((err, post) => {
-        // REDIRECT TO THE ROOT
+      post.save(function(err, post) {
         return res.redirect(`/`);
-      })
-    });
+      });
+    } else {
+      return res.status(401); // UNAUTHORIZED
+    }
+  });
 
   // SHOW SINGLE POST
     app.get("/posts/:id", function(req, res) {
-      // LOOK UP THE POST
-  Post.findById(req.params.id).populate('comments').then((post) => {
-    res.render('post-show', { post })
-  }).catch((err) => {
-    console.log(err.message)
-  })
-  });
+    // LOOK UP THE POST
+      Post.findById(req.params.id).populate('comments').then((post) => {
+        res.render('post-show', { post })
+      }).catch((err) => {
+        console.log(err.message)
+      })
+    });
 
   // SUBREDDIT
     app.get("/n/:subreddit", function(req, res) {
--- server.js
+++ server.js
@@ -25,6 +25,20 @@ const hbs = exphbs.create({
     handlebars: allowInsecurePrototypeAccess(handlebars),
 });
 
+var checkAuth = (req, res, next) => {
+  console.log("Checking authentication");
+  if (typeof req.cookies.nToken === "undefined" || req.cookies.nToken === null) {
+    req.user = null;
+  } else {
+    var token = req.cookies.nToken;
+    var decodedToken = jwt.decode(token, { complete: true }) || {};
+    req.user = decodedToken.payload;
+  }
+
+  next();
+};
+app.use(checkAuth);
+
 
 // Set db
 require('./data/reddit-db');
