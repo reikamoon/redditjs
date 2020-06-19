67889c78323501592da07cb058779fc858735be8
Anjelica <ambrosio.anjelica@gmail.com>
2020-06-18 18:03:00 -0700

    users can log out

--- controllers/auth.js
+++ controllers/auth.js
@@ -21,4 +21,11 @@ module.exports = (app) => {
       return res.status(400).send({ err: err });
     });
 });
+
+// LOGOUT
+ app.get('/logout', (req, res) => {
+   res.clearCookie('nToken');
+   res.redirect('/');
+ });
+ 
 };
