de996961666a7c950a8ddc24df2ef2569c7e1fcd
Anjelica <ambrosio.anjelica@gmail.com>
2020-06-23 11:22:52 -0700

    fixed posts tests

--- test/posts.js
+++ test/posts.js
@@ -8,6 +8,7 @@ const expect = chai.expect;
 // we can use it in our tests.
 const Post = require('../models/post');
 const server = require('../server');
+const agent = chai.request.agent(app);
 
 chai.should();
 chai.use(chaiHttp);
@@ -20,6 +21,22 @@ describe('Posts', function() {
       url: 'https://www.google.com',
       summary: 'post summary'
   };
+  const user = {
+    username: 'poststest',
+    password: 'testposts'
+};
+before(function (done) {
+  agent
+    .post('/sign-up')
+    .set("content-type", "application/x-www-form-urlencoded")
+    .send(user)
+    .then(function (res) {
+      done();
+    })
+    .catch(function (err) {
+      done(err);
+    });
+});
   it('Should create with valid attributes at POST /posts/new', function(done) {
   // Checks how many posts there are now
   Post.estimatedDocumentCount()
@@ -52,7 +69,23 @@ describe('Posts', function() {
         done(err);
     });
 });
-after(function () {
-  Post.findOneAndDelete(newPost);
+after(function (done) {
+  Post.findOneAndDelete(newPost)
+  .then(function (res) {
+      agent.close()
+
+      User.findOneAndDelete({
+          username: user.username
+      })
+        .then(function (res) {
+            done()
+        })
+        .catch(function (err) {
+            done(err);
+        });
+  })
+  .catch(function (err) {
+      done(err);
+  });
 });
   });
