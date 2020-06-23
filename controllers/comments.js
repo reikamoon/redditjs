ee411ae5d6ec233aef20af7c5ceb779da170a766
Anjelica <ambrosio.anjelica@gmail.com>
2020-06-23 11:20:53 -0700

    implemented auth tests

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,37 +1,30 @@
-const Post = require('../models/post');
 const Comment = require('../models/comment');
-const Comment = new Comment(req.body);
-comment.author = req.user._id;
+const User = require('../models/user');
+const Post = require('../models/post');
 
 module.exports = function(app) {
-  // CREATE Comment
-  app.post("/posts/:postId/comments", function(req, res) {
-    if (req.user) {
-      const comment = new Comment(req.body);
-      comment.save(function(err,comment) {
-        return res.redirect('/');
-      });
-    } else {
-      return res.status(401); ///UNAUTHORIZED
-    }
-    // SAVE INSTANCE OF Comment MODEL TO DB
-    comment
-      .save()
-      .then(comment => {
-        return Post.findById(req.params.postId);
-      })
-      .then(post => {
-        post.comments.unshift(comment);
-        return post.save();
-      })
-      .then(post => {
-        res.redirect(`/`);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
 
-
+    // CREATE Comment
+    app.post("/posts/:postId/comments", function(req, res) {
+        // INSTANTIATE INSTANCE OF MODEL
+        const comment = new Comment(req.body);
+        comment.author = req.user._id;
+        // SAVE INSTANCE OF Comment MODEL TO DB
+        comment
+            .save()
+            .then(comment => {
+                return Post.findById(req.params.postId);
+        })
+            .then(post => {
+                post.comments.unshift(comment);
+                return post.save();
+        })
+            .then(post => {
+                res.redirect(`/posts/${req.params.postId}`);
+        })
+            .catch(err => {
+                console.log(err);
+        });
+    });
 
 };
